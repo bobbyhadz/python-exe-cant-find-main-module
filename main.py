9d2503cf075f46b52a4aee29c5a3d5c0beaaf20e
bobbyhadz <opmftw@gmail.com>
2024-04-13 07:55:34 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# python.exe: can't find '__main__' module in Path 
+
+def do_work():
+    print('bobbyhadz.com')
+
+
+if __name__ == '__main__':
+    # call functions or run logic here 👇️
+    do_work()
